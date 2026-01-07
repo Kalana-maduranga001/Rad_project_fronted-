6248899652225a79026e8e8e5a3b87112a1c2286
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-07 11:35:49 +0530

    all errors debugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const { user } = useAuth()
   return (
     <div className="p-6 bg-white shadow-lg rounded-xl">
-        <h1 className="text-2xl font-bold text-gray-800">Welcome, {user?.name}</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Welcome, {user?.username}</h1>
         <p className="text-gray-600 mt-2">You're logged in successfully.</p>
     </div>
     )
